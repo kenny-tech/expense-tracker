1071f4c1b932e3e9fa42b9002262e67ce716c609
kenny-tech <kennyemma2008@gmail.com>
2020-08-26 10:01:41 +0100

    Added scroll view to Income screen

--- app/screens/Income.js
+++ app/screens/Income.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, ScrollView, KeyboardAvoidingView} from 'react-native';
 
 import FormView from '../components/FormView';
 import Mytextinput from '../components/Mytextinput';
@@ -8,20 +8,28 @@ const Income = () => {
     const [amount, setAmount] = useState('');
     const [note, setNote] = useState('');
 
+
     return (
         <View style={{flex: 1, alignItems: 'center'}}>
-            <FormView label="Type" />
-            <FormView 
-                label="Amount" 
-                inputType={<Mytextinput placeholder="Amount" keyboardType="numeric" onChangeText={amount => setAmount(amount)}/>}
-            />
-            <FormView label="Category"/>
-            <FormView label="Date & Time"/>
-            <FormView label="Mode"/>
-            <FormView 
-                label="Note"
-                inputType={<Mytextinput placeholder="Note" onChangeText={note => setNote(note)}/>}
-            />
+            <ScrollView keyboardShouldPersistTaps='handled'>
+                <KeyboardAvoidingView
+                    behavior="padding"
+                    style={{ flex: 1, justifyContent: 'space-between' }}
+                >
+                    <FormView label="Type" />
+                    <FormView 
+                        label="Amount" 
+                        inputType={<Mytextinput placeholder="Amount" keyboardType="numeric" onChangeText={amount => setAmount(amount)}/>}
+                    />
+                    <FormView label="Category"/>
+                    <FormView label="Date & Time"/>
+                    <FormView label="Mode"/>
+                    <FormView 
+                        label="Note"
+                        inputType={<Mytextinput placeholder="Note" onChangeText={note => setNote(note)}/>}
+                    />
+                </KeyboardAvoidingView>
+            </ScrollView>
         </View>
     )
 }
