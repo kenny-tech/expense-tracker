a172e46a9b512e4d368a6dfa75adb9c266d4558e
kenny-tech <kennyemma2008@gmail.com>
2020-09-24 13:56:32 +0100

    Fixed chart bug

--- app/components/PieChart.js
+++ app/components/PieChart.js
@@ -1,24 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { VictoryPie } from 'victory-native';
 
 import styles from '../styles/style';
 
 const PieChart = ({ month, year, income, expense }) => {
-    console.log('income and expense here:  '+income, expense);
     let percentIncome = ((income / (income + expense)) * 100).toFixed(2);
     let percentExpense = ((expense / (income + expense)) * 100).toFixed(2);
-    console.log('income and expense percents: '+percentIncome, percentExpense);
     const graphicColor = ['#006400', '#C70039'];
-    const wantedGraphicData = [{ y: income, x: percentIncome+'%' }, { y: expense, x: percentExpense+'%' }]; // Data that we want to display
-    const defaultGraphicData = [{ y: 0, x: '' }, { y: 0, x: '' }]; // Data used to make the animate prop work
-
-    const [graphicData, setGraphicData] = useState(defaultGraphicData);
-
-    useEffect(() => {
-        setGraphicData(wantedGraphicData); // Setting the data that we want to display
-    });
-
+    const graphicData = [{ y: income, x: percentIncome+'%' }, { y: expense, x: percentExpense+'%' }]; // Data that we want to display
+    
     return (
         <View style={styles.pieChartView}>
             <Text style={{
