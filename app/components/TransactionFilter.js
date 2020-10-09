e810ab498cf6b53a2413a884c87eb71ddc316fdd
kenny-tech <kennyemma2008@gmail.com>
2020-10-09 13:21:32 +0100

    Made Filter By title dynamic

---
+++ app/components/TransactionFilter.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import styles from '../styles/style';
+import { DB } from '../model/db';
+import TransactionMonth from '../components/TransactionMonth';
+import NoTransaction from '../components/NoTransaction';
+
+const TransactionFilter = ({ filterBy }) => {
+    
+    const [transactions, setTransactions] = useState([]);
+   
+    let month = new Date().getMonth()+1;     
+    let monthNumber
+    
+    if(month == 1) {
+        monthNumber = '01';
+    }
+    if(month == 2) {
+        monthNumber = '02';
+    }
+    if(month == 3) {
+        monthNumber = '03';
+    }
+    if(month == 4) {
+        monthNumber = '04';
+    }
+    if(month == 5) {
+        monthNumber = '05';
+    }
+    if(month == 6) {
+        monthNumber = '06';
+    }
+    if(month == 7) {
+        monthNumber = '07';
+    }
+    if(month == 8) {
+        monthNumber = '08';
+    }
+    if(month == 9) {
+        monthNumber = '09';
+    }
+
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const convertDate = (date_str) => {
+        let temp_date = date_str.split("-");
+        return temp_date[2] + " " + months[Number(temp_date[1]) - 1] + " " + temp_date[0];
+    }
+
+    const getTransactions = () => {
+        DB.transaction(tx => {
+            // tx.executeSql(`SELECT * FROM transactions WHERE strftime('%m', date) = ?`, [monthNumber], (tx, results) => {
+            tx.executeSql(`SELECT * FROM transactions ORDER BY rowid DESC`, [], (tx, results) => {
+                let temp = [];
+                for (let i = 0; i < results.rows.length; ++i) {
+                    temp.push(results.rows.item(i));
+                }
+                console.log('Transactions: ',temp);
+                setTransactions(temp);
+            })
+        });
+    }
+
+    const numberWithCommas = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    useEffect(() => {
+        getTransactions()
+    }, []);
+
+    return (
+        <View> 
+            <TransactionMonth monthName={filterBy}/>
+            {
+                <View>
+                    {
+                        transactions.length != 0 ? (<FlatList
+                            data={transactions}
+                            renderItem={({ item }) => (
+                                <View>
+                                    <View style={{flexDirection: 'row'}}>
+                                        <Icon name="money" size={30} color="#4b81bf" style={{marginTop: 20, marginHorizontal: 20}} />
+                                        <View style={styles.transactionViewText}>
+                                            {
+                                                item.type == 'Income' ? (<Text style={{color: '#006400', fontSize: 18}}>{numberWithCommas(item.amount)}</Text>) :  (<Text style={{color: '#C70039', fontSize: 18}}>{numberWithCommas(item.amount)}</Text>)
+                                            }
+                                            <Text style={{fontStyle: 'italic'}}>{convertDate(item.date)}</Text>
+                                        </View>  
+                                        <Icon name="angle-right" size={30} color="#4b81bf" style={{marginTop: 20, marginLeft: 200}} /> 
+                                    </View>
+                                    <View style={{borderBottomWidth: 1, marginHorizontal: 20, width: 350, padding: 5, borderColor: '#d3d3d3'}}/>
+                                </View>
+                            )}
+                            keyExtractor={item => item.rowid}
+                        />) : (<View style={{marginTop: 50}}>
+                                    <NoTransaction/>
+                                    <Text style={{textAlign: 'center', fontSize: 16}}>Please add a transaction</Text>
+                                </View>)
+                    }
+                </View>
+            }
+            
+             <View style={styles.bottomView}>
+                <Text style={styles.bottomViewText}>Balance for {filterBy}: NGN20,000.00 </Text>
+            </View>
+        </View>
+    )
+}
+
+export default TransactionFilter;
--- app/screens/Transactions.js
+++ app/screens/Transactions.js
@@ -4,12 +4,12 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Dialog, { SlideAnimation, DialogContent, DialogTitle, DialogFooter, DialogButton } from 'react-native-popup-dialog';
 
 import TransactionText from '../components/TransactionText';
+import TransactionFilter from '../components/TransactionFilter';
 import styles from '../styles/style';
 import FormView from '../components/FormView';
 import Myselectinput from '../components/Myselectinput';
 import { DB } from '../model/db';
 import DateRange from '../components/DateRange';
-import DateRangeButton from '../components/DateRangeButton';
 
 const Transactions = ({ navigation }) => {
 
@@ -20,6 +20,7 @@ const Transactions = ({ navigation }) => {
     const [dateFrom, setDateFrom] = useState('');
     const [dateTo, setDateTo] = useState('');
     const [maxDate, setMaxDate] = useState('');
+    const [filter, setFilter] = useState(false);
 
     useEffect(() => {
         navigation.setOptions({
@@ -62,14 +63,23 @@ const Transactions = ({ navigation }) => {
     const handleFilterBy = (filter_by) => { 
         if(filter_by == 'Date Range') {
             setShowDateRange(true);
+            // filter_by = `${dateFrom} - ${dateTo}`;
         } else {
             setShowDateRange(false);
         }
         setFilterBy(filter_by);
     }
 
+    const handleFilterTransaction = () => {
+        setVisible(false);
+        setFilter(true);
+    }
+
     return (
         <View style={styles.transactionView}>
+            {
+                filter? (<TransactionFilter filterBy={filterBy}/>) : (<TransactionText/>)
+            }
             <TransactionText/>
             <Dialog
                 visible={visible}
@@ -88,7 +98,7 @@ const Transactions = ({ navigation }) => {
                       />
                       <DialogButton
                         text="OK"
-                        onPress={() => {}}
+                        onPress={() => handleFilterTransaction()}
                       />
                     </DialogFooter>
                 }
@@ -112,7 +122,6 @@ const Transactions = ({ navigation }) => {
                                 >
                                     <DateRange defaultDate={dateFrom} onDateChange={(dateFrom) => {setDateFrom(dateFrom)}} label="From" maxDate={maxDate}/>
                                     <DateRange defaultDate={dateTo} onDateChange={(dateTo) => {setDateTo(dateTo)}} label="To" maxDate={maxDate}/>
-                                    <DateRangeButton title="Go" customClick={() => handleSubmit()}/>
                                 </View>
                             ) : null
                         }
