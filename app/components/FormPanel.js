3393ea896f45a41cb6ec8e953ab9d6e704a67cb8
kenny-tech <kennyemma2008@gmail.com>
2020-11-27 18:39:57 +0100

    Formatted manage category view

--- app/components/FormPanel.js
+++ app/components/FormPanel.js
@@ -11,11 +11,11 @@ const FormPanel = ({ types, customPress }) => {
                 types.map(type => {
                     return (
                         <TouchableOpacity onPress={() => customPress(type.rowid)}>
-                            <View style={styles.formViewPanel}>
-                                <View>
+                            <View style={styles.formViewPanel} key={type.rowid}>
+                                <View style={{width: '80%'}}>
                                     <Text style={{fontSize: 18, margin: 15}}>{type.name}</Text>
                                 </View>
-                                <View style={{marginLeft: 130}}>
+                                <View style={{width: '20%'}}>
                                     <Icon name="pencil" size={30} color="#4b81bf" style={{marginTop: 10, marginLeft: 20}} /> 
                                 </View>
                             </View>
